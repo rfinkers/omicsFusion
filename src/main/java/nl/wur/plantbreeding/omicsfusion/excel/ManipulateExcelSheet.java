f7249cb538eb6f55daa921a6a3fb05915c86726a
finke002 <finke002@c6b4c25d-3c9f-4c26-b924-d08501b13b84>
2011-10-13 18:54:34 +0000

    Wrapper code for ExcelSheetUploads

--- src/main/java/nl/wur/plantbreeding/omicsfusion/excel/ManipulateExcelSheet.java
+++ src/main/java/nl/wur/plantbreeding/omicsfusion/excel/ManipulateExcelSheet.java
@@ -21,12 +21,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.logging.Logger;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.DateUtil;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellReference;
 
 /**
--- src/main/java/nl/wur/plantbreeding/omicsfusion/excel/UploadDataSheets.java
+++ src/main/java/nl/wur/plantbreeding/omicsfusion/excel/UploadDataSheets.java
@@ -16,19 +16,75 @@
 package nl.wur.plantbreeding.omicsfusion.excel;
 
 import java.io.File;
+import java.io.IOException;
+import java.sql.Time;
+import java.util.Timer;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 
 /**
  * Parses the information in the excel sheets and uploads this information to
  * the SQLite database.
+ *
  * @author Richard Finkers
  * @version 1.0
  *
  */
-public class UploadDataSheets {
+public class UploadDataSheets extends ManipulateExcelSheet {
 
-    public static void uploadExcelSheets(File responseSheet,
-            String responseType, File predictorSheet, String predictorType) {
+    public static void uploadExcelSheets(File responseExcelFile,
+            String responseType, File predictorExcelFile, String predictorType) {
+
+        /**
+         * wb for the response variables.
+         */
+        Workbook responseWorkbook = null;
+        /**
+         * wb for the predictor variables.
+         */
+        Workbook predictorWorkbook = null;
+
+        try {
+            responseWorkbook = loadExcelSheet(responseExcelFile);
+
+            predictorWorkbook = loadExcelSheet(predictorExcelFile);
+        }
+        catch (IOException iOException) {
+        }
+        catch (InvalidFormatException invalidFormatException) {
+        }
+
+        //First row contains the variable
+        Sheet responseSheet = responseWorkbook.getSheetAt(0);
+        Sheet predictorSheet = predictorWorkbook.getSheetAt(0);
+        Row responseHeaderRow = responseSheet.getRow(0);
+        Row predictorHeaderRow = predictorSheet.getRow(0);
+        //Second row can contain the ontology ID (idetified with column name)
+        String ontologyID = "ontology";
+        int responseRowCounter = 1;
+        int predictorRowCounter = 1;
+        if (responseSheet.getRow(responseRowCounter).getCell(0).
+                getRichStringCellValue().getString().equals(ontologyID)) {
+            responseRowCounter++;
+            //parse row to db
+        }
+        if (predictorSheet.getRow(predictorRowCounter).getCell(0).
+                getRichStringCellValue().getString().equals(ontologyID)) {
+            predictorRowCounter++;
+            //parse row to DB
+        }
+        //Data (first column genotype, other columns data).
+        //parse the rest of the data.
+        for (int i = responseRowCounter;
+                i < responseSheet.getLastRowNum(); i++) {
+        }
+
+        for (int i = predictorRowCounter;
+                i < predictorSheet.getLastRowNum(); i++) {
+        }
+
         throw new UnsupportedOperationException("Not yet implemented");
     }
-
 }
