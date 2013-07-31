6bde6217b575c4cfd36a7f5a7fe9acfd3f4b5aac
Richard Finkers <richard.finkers@gmail.com>
2013-07-31 20:16:07 +0200

    Addaptation of datatypes to new schema and required information.

--- src/main/java/nl/wur/plantbreeding/omicsfusion/datatypes/DataPointDataType.java
+++ src/main/java/nl/wur/plantbreeding/omicsfusion/datatypes/DataPointDataType.java
@@ -26,9 +26,17 @@ public class DataPointDataType {
      */
     private String genotypeName;
     /**
-     * Name of the trait.
+     * Number of the genotype.
      */
-    private String traitName;
+    private Integer genotypeID;
+    /**
+     * Name of the observation.
+     */
+    private String observationName;
+    /**
+     * Number of the observation.
+     */
+    private String observationID;
     /**
      * The observation.
      */
@@ -37,9 +45,21 @@ public class DataPointDataType {
     public DataPointDataType() {
     }
 
-    public DataPointDataType(String genotypeName, String traitName, double observation) {
+    /**
+     * Constructor to fill in the complete object.
+     *
+     * @param genotypeName Name of the genotype.
+     * @param genotypeID ID of the genotype.
+     * @param observationName Name of the predictor or response variable.
+     * @param observationID ID of the predictor of the response variable.
+     * @param observation The associated observation.
+     */
+    public DataPointDataType(String genotypeName, Integer genotypeID,
+            String observationName, String observationID, double observation) {
         this.genotypeName = genotypeName;
-        this.traitName = traitName;
+        this.genotypeID = genotypeID;
+        this.observationName = observationName;
+        this.observationID = observationID;
         this.observation = observation;
     }
 
@@ -59,17 +79,33 @@ public class DataPointDataType {
         this.observation = observation;
     }
 
-    public String getTraitName() {
-        return traitName;
+    public Integer getGenotypeID() {
+        return genotypeID;
     }
 
-    public void setTraitName(String traitName) {
-        this.traitName = traitName;
+    public void setGenotypeID(Integer genotypeID) {
+        this.genotypeID = genotypeID;
+    }
+
+    public String getObservationName() {
+        return observationName;
+    }
+
+    public void setObservationName(String observationName) {
+        this.observationName = observationName;
+    }
+
+    public String getObservationID() {
+        return observationID;
+    }
+
+    public void setObservationID(String observationID) {
+        this.observationID = observationID;
     }
 
     @Override
     public String toString() {
-        return "DataPointDataType{" + "genotypeName=" + genotypeName + ", traitName=" + traitName + ", observation=" + observation + '}';
+        return "DataPointDataType{" + "genotypeName=" + genotypeName + ", traitName=" + observationName + ", observation=" + observation + '}';
     }
 
     @Override
@@ -84,7 +120,7 @@ public class DataPointDataType {
         if ((this.genotypeName == null) ? (other.genotypeName != null) : !this.genotypeName.equals(other.genotypeName)) {
             return false;
         }
-        if ((this.traitName == null) ? (other.traitName != null) : !this.traitName.equals(other.traitName)) {
+        if ((this.observationName == null) ? (other.observationName != null) : !this.observationName.equals(other.observationName)) {
             return false;
         }
         if (Double.doubleToLongBits(this.observation) != Double.doubleToLongBits(other.observation)) {
@@ -97,7 +133,7 @@ public class DataPointDataType {
     public int hashCode() {
         int hash = 3;
         hash = 83 * hash + (this.genotypeName != null ? this.genotypeName.hashCode() : 0);
-        hash = 83 * hash + (this.traitName != null ? this.traitName.hashCode() : 0);
+        hash = 83 * hash + (this.observationName != null ? this.observationName.hashCode() : 0);
         hash = 83 * hash + (int) (Double.doubleToLongBits(this.observation) ^ (Double.doubleToLongBits(this.observation) >>> 32));
         return hash;
     }
