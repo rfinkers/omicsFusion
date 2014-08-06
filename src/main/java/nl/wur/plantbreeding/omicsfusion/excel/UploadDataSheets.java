e6be231f25d3e3c7d9b3561428c262d9818fa99e
Richard Finkers <richard.finkers@gmail.com>
2014-08-06 15:07:49 +0200

    Remove redundant code & Fix bug in reading excel sheet.

--- src/main/java/nl/wur/plantbreeding/omicsfusion/excel/UploadDataSheets.java
+++ src/main/java/nl/wur/plantbreeding/omicsfusion/excel/UploadDataSheets.java
@@ -25,7 +25,6 @@ import nl.wur.plantbreeding.logic.sqlite.SqLiteQueries;
 import nl.wur.plantbreeding.omicsfusion.datatypes.DataPointDataType;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
@@ -65,7 +64,6 @@ public class UploadDataSheets extends ManipulateExcelSheet {
 
         try {
             responseWorkbook = loadExcelSheet(responseExcelFile);
-
             predictorWorkbook = loadExcelSheet(predictorExcelFile);
         } catch (IOException iOException) {
             System.out.println("IO");
@@ -73,8 +71,6 @@ public class UploadDataSheets extends ManipulateExcelSheet {
             System.out.println("Format");
         }
 
-        List<DataPointDataType> rdp = null;
-        int predictorRowCounter = 0;
         Sheet predictorSheet = null;
         Sheet responseSheet = null;
 
@@ -87,20 +83,14 @@ public class UploadDataSheets extends ManipulateExcelSheet {
             //TODO
         }
 
-        Row responseHeaderRow = responseSheet.getRow(0);
-        //TODO: miss one row!. +1 leads to NPE!
-        int responseRowLenght = responseHeaderRow.getLastCellNum();
-        Row predictorHeaderRow = predictorSheet.getRow(0);
-        int predictorRowLength = predictorHeaderRow.getLastCellNum();
         //Second row can contain the ontology ID (idetified with column name)
         final String ontologyID = "ontology";
         int responseRowCounter = 1;
-        predictorRowCounter = 1;
+        int predictorRowCounter = 1;
         try {
             if (responseSheet.getRow(responseRowCounter).getCell(0).getRichStringCellValue().getString().equals(ontologyID)) {
                 responseRowCounter++;
-            //parse row to db
-
+                //parse row to db
                 //FIXME: will introduce a bug?
                 //variable, ontologyID
             }
@@ -115,76 +105,85 @@ public class UploadDataSheets extends ManipulateExcelSheet {
         }
         //Data (first column genotype, other columns data).
         //parse the rest of the data.
-        rdp = new ArrayList<>();
+        /**
+         * List with response variables.
+         */
+        List<DataPointDataType> rdp = new ArrayList<>();
+        /**
+         * List with predictor variables.
+         */
+        List<DataPointDataType> pdp = new ArrayList<>();
+        /**
+         * List with traits.
+         */
         HashMap<String, String> resp = new HashMap<>();
 
-        for (int i = responseRowCounter;
-                i < responseSheet.getLastRowNum() + 1; i++) {
-
-            String trait;
-            Double observation;
-            String genotype;
-            if (responseSheet.getRow(i).getCell(0).getCellType() == Cell.CELL_TYPE_NUMERIC) {
-                genotype = Double.toString(responseSheet.getRow(i).getCell(0).getNumericCellValue());
-            } else {
-                genotype = responseSheet.getRow(i).getCell(0).getStringCellValue().trim();
-            }
-            for (int j = 1; j < responseRowLenght; j++) {
-                if (!responseHeaderRow.getCell(j).getStringCellValue().isEmpty()) {
-                    trait = responseHeaderRow.getCell(j).getStringCellValue();
-                    if (responseSheet.getRow(i).getCell(j).getCellType()
-                            == Cell.CELL_TYPE_NUMERIC) {
-                        try {
-                            observation = responseSheet.getRow(i).getCell(j).getNumericCellValue();
-                        } catch (Exception e) {
-                            observation = Double.NaN;
-                        }
-                    } else {
-                        observation = Double.NaN;
-                    }
-                    rdp.add(new DataPointDataType(genotype, i, trait, "t" + j, observation));
-                    if (i == responseRowCounter) {
-                        System.out.println("Trait: " + trait);
-                        resp.put("t" + j, trait);
-                    }
-                }
-            }
-        }
-
-        List<DataPointDataType> pdp = new ArrayList<>();
-
-        //TODO: This logic is redundant! Refractor out to separate method.
-        for (int i = predictorRowCounter;
-                i < predictorSheet.getLastRowNum() + 1; i++) {
-
-            String header;
-            Double value;
-            String genotype;
-            if (predictorSheet.getRow(i).getCell(0).getCellType() == Cell.CELL_TYPE_NUMERIC) {
-                genotype = Double.toString(predictorSheet.getRow(i).getCell(0).getNumericCellValue());
-            } else {
-                genotype = predictorSheet.getRow(i).getCell(0).getStringCellValue().trim();
-            }
-            for (int j = 1; j < predictorRowLength; j++) {
-                if (!predictorHeaderRow.getCell(j).getStringCellValue().isEmpty()) {
-                    header = predictorHeaderRow.getCell(j).getStringCellValue();
-                    if (predictorSheet.getRow(i).getCell(j).getCellType()
-                            == Cell.CELL_TYPE_NUMERIC) {
-                        try {
-                            value = predictorSheet.getRow(i).getCell(j).getNumericCellValue();
-                        } catch (Exception e) {
-                            value = Double.NaN;
-                        }
-                    } else {
-                        value = Double.NaN;
-                    }
-                    pdp.add(new DataPointDataType(genotype, i, header, "p" + j, value));
-                }
-
-            }
-        }
+        //Fill the corresponding data objects.
+        rdp = parseDataCells(responseRowCounter, responseSheet, "t");
+        pdp = parseDataCells(predictorRowCounter, predictorSheet, "p");
+        resp = parseTraitList(responseSheet);
 
         SqLiteQueries sql = new SqLiteQueries();
         sql.loadExcelData(rdp, pdp, resp, directory);
+    }
+
+    /**
+     * Parses the numeric values, from the EXCEL sheets, and load them in a
+     * list. *
+     */
+    private static List<DataPointDataType> parseDataCells(int rowCounter, Sheet excelSheet, String pre) {
+        List<DataPointDataType> dataPoints = new ArrayList<>();
+        for (int i = rowCounter;
+                i < excelSheet.getLastRowNum() + 1; i++) {
+            String header;
+            Double value;
+            String genotype;
+            //Read the name of the genotype
+            if (excelSheet.getRow(i).getCell(0).getCellType() == Cell.CELL_TYPE_NUMERIC) {
+                genotype = Double.toString(excelSheet.getRow(i).getCell(0).getNumericCellValue());
+            } else {
+                genotype = excelSheet.getRow(i).getCell(0).getStringCellValue().trim();
+            }
+
+            for (int j = 1; j < excelSheet.getRow(0).getLastCellNum(); j++) {
+                if (!excelSheet.getRow(0).getCell(j).getStringCellValue().isEmpty()) {
+                    header = excelSheet.getRow(0).getCell(j).getStringCellValue();
+                    //TODO: Test the following
+                    //Valus could also be as text? This already leads to an now uncatched error in the next line.
+                    //this is redundant with the same code in the method above -> Refractor out
+                    //Use SWITCH to go through the different options and act accordingly.
+                    //FIXME: miss one row!. +1 leads to NPE!??? Is this still teh case after refractoring?
+                    switch (excelSheet.getRow(i).getCell(j).getCellType()) {
+                        case (Cell.CELL_TYPE_NUMERIC):
+                            value = excelSheet.getRow(i).getCell(j).
+                                    getNumericCellValue();
+                            break;
+                        case (Cell.CELL_TYPE_STRING):
+                            value = Double.parseDouble(excelSheet.getRow(i).
+                                    getCell(j).getStringCellValue());
+                            break;
+                        default:
+                            value = Double.NaN;
+                    }
+                    dataPoints.add(new DataPointDataType(genotype, i, header, pre + j, value));
+                }
+            }
+        }
+        return dataPoints;
+    }
+
+    /**
+     * Parse the list of traits.
+     */
+    private static HashMap<String, String> parseTraitList(Sheet excelSheet) {
+        HashMap<String, String> traitNames = new HashMap<>();
+        for (int j = 1; j < excelSheet.getRow(0).getLastCellNum(); j++) {
+            if (!excelSheet.getRow(0).getCell(j).getStringCellValue().isEmpty()) {
+                String header = excelSheet.getRow(0).getCell(j).getStringCellValue();
+                System.out.println("Trait: " + header);
+                traitNames.put("t" + j, header);
+            }
+        }
+        return traitNames;
     }
 }
